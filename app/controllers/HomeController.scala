df7dde65c7fb407edc6861fe6f9aa4fd2b1045f6
ayub96 <ayusuf@academytrainee.com>
2021-06-28 15:55:41 +0100

    AY: Login check complete

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,29 @@
 package controllers
 
+import models.{AgentLogin}
+
 import javax.inject._
 import play.api._
+import play.api.libs.json.JsSuccess
 import play.api.mvc._
+import repos.AgentLoginRepo
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents, agentLoginRepo: AgentLoginRepo) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def checkAgentLogin() = Action.async(parse.json) { implicit request =>
+    request.body.validate[AgentLogin] match {
+      case JsSuccess(agentLogin, _) => agentLoginRepo.checkAgent(agentLogin).map {
+        case true => Ok("Agent Login found!")
+        case false => BadRequest("Agent Login not found!")
+      }
+    }
   }
 }
