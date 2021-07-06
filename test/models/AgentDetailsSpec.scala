d1671b1ff745236ec70124fd8dbff09a127d6169
“QAcpardeep” <cpardeep@qa.com>
2021-07-06 11:24:19 +0100

    added in test for agentDetails model

---
+++ test/models/AgentDetailsSpec.scala
@@ -0,0 +1,30 @@
+package models
+
+import play.api.libs.json.{JsSuccess, JsValue, Json}
+
+class AgentDetailsSpec extends AbstractModelsTest {
+
+  val agentDetailsModel: AgentDetails = AgentDetails(arn = "ARN51226238", businessName = "testBusinessName", email = "test@gmail.com", mobileNumber = "98765".toInt, moc = List("test"), propertyNumber = "test", postcode = "test")
+  val agentDetailsJs: JsValue = Json.parse("""{
+                                             |    "arn" : "ARN51226238",
+                                             |    "businessName" : "testBusinessName",
+                                             |    "email" : "test@gmail.com",
+                                             |    "mobileNumber" : 98765,
+                                             |    "moc" : [
+                                             |        "test"
+                                             |    ],
+                                             |    "propertyNumber" : "test",
+                                             |    "postcode" : "test"
+                                             |}
+                                             |""".stripMargin)
+
+  "AgentDetails" should {
+    "format from case class to json" in {
+      Json.toJson(agentDetailsModel) shouldBe agentDetailsJs
+    }
+    "format from json to case class" in {
+      Json.fromJson[AgentDetails](agentDetailsJs) shouldBe JsSuccess(agentDetailsModel)
+    }
+  }
+
+}
