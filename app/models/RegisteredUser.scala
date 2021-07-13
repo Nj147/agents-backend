4a319572305536603640240915bad52c852bbf8d
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2021-07-13 10:21:23 +0100

    added case class for update correspondence

--- app/models/RegisteredUser.scala
+++ app/models/RegisteredUser.scala
@@ -31,3 +31,9 @@ case class AgentCheck(arn: String)
 object AgentCheck{
   implicit val format: OFormat[AgentCheck] = Json.format[AgentCheck]
 }
+
+case class UpdateCorrespondence(arn: String, moc: List[String])
+
+object UpdateCorrespondence{
+  implicit val format: OFormat[UpdateCorrespondence] = Json.format[UpdateCorrespondence]
+}
