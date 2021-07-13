963e6eb3aa558732b4eb7f75e0301eeeefa3da4c
ISTANBULBEKLE <ekipkalir@gmail.com>
2021-07-13 13:39:40 +0100

    ChangeAgentDetailsControllerSpec tests are done, patch routes for update-email is done

--- test/controllers/ChangeAgentDetailsControllerSpec.scala
+++ test/controllers/ChangeAgentDetailsControllerSpec.scala
@@ -145,4 +145,29 @@ class ChangeAgentDetailsControllerSpec extends AbstractControllerTest {
     }
   }
 
+  "updateEmail;  /update-email" should {
+    "return an accepted status" when {
+      "the received JsValue is a valid agent email address and the update is successful" in {
+        when(repo.updateEmail(any())) thenReturn(Future.successful(true))
+        val result = controller.updateEmail().apply(FakeRequest("PATCH", "/update-email").withBody(Json.toJson(agentEmail)))
+        status(result) shouldBe ACCEPTED
+      }
+    }
+
+    "return an unacceptable status" when {
+      "the received JsValue is a valid agent email address but nothing in the database is updated" in {
+        when(repo.updateEmail(any())) thenReturn Future.successful(false)
+        val result = controller.updateEmail().apply(FakeRequest("PATCH", "/update-email").withBody(Json.toJson(agentEmail)))
+        status(result) shouldBe NOT_ACCEPTABLE
+      }
+    }
+
+    "return a bad request status" when {
+      "the received JsValue is not a valid agent email" in {
+        val result = controller.updateEmail().apply(FakeRequest().withBody(Json.toJson("" -> "")))
+        status(result) shouldBe BAD_REQUEST
+      }
+    }
+  }
+
 }
